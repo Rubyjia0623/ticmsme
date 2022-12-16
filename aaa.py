e20b681988164193b43e6ed35c66e8eb669983f6
Rubyjia0623 <113332952+Rubyjia0623@users.noreply.github.com>
2022-12-16 21:51:01 +0800

    Update aaa.py

--- aaa.py
+++ aaa.py
@@ -1,7 +1,7 @@
 import firebase_admin
 import random
 from firebase_admin import credentials, firestore
-cred = credentials.Certificate("riddle.json")
+cred = credentials.Certificate("serviceAccountKey.json")
 firebase_admin.initialize_app(cred)
 db = firestore.client()
 
